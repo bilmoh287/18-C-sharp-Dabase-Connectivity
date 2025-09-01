78dd508f8029efcada74288863a4519525bc77ef
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-09-01 13:52:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Dabase_Connectivity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
