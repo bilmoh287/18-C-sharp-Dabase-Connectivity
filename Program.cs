1a7499afbc81205d97367aa436980d955993b864
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-09-02 09:02:09 +0300

    First Commit Read Data from SQL

--- Program.cs
+++ Program.cs
@@ -1,15 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Net;
+using System.Data.SqlClient;
 
 namespace C__Dabase_Connectivity
 {
     internal class Program
     {
+        static string ConnectionString = "Server=localhost\\SQL22;Database=ContactsDB;Trusted_Connection=True;";
+
+        static void PrintContact()
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+
+            string query = "SELECT * FROM Contacts";
+
+            SqlCommand command = new SqlCommand(query, connection); 
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while(reader.Read())
+                {
+
+                    int ContactID = (int)reader["ContactID"];
+                    string FirstName = (string)reader["FirstName"];
+                    string LasttName = (string)reader["LastName"];
+                    string Email = (string)reader["Email"];
+                    string Phone = (string)reader["Phone"];
+                    string Address = (string)reader["Address"];
+                    int CountryID = (int)reader["CountryID"];
+
+                    Console.WriteLine($"ContactID :  {ContactID}");
+                    Console.WriteLine($"FirstName :  {FirstName}");
+                    Console.WriteLine($"LastName  :  {LasttName}");
+                    Console.WriteLine($"Email     :  {Email}");
+                    Console.WriteLine($"Phone     :  {Phone}");
+                    Console.WriteLine($"Address   :  {Address}");
+                    Console.WriteLine($"CountryID :  {CountryID}");
+                    Console.WriteLine();
+
+                }
+
+                reader.Close();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
         static void Main(string[] args)
         {
+            PrintContact();
+            Console.ReadKey();
         }
     }
 }
